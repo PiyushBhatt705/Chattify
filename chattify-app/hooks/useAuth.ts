9d85b1851396516e38186abd207438c17d6c6e48
Piyush Bhatt <134905218+PiyushBhatt705@users.noreply.github.com>
2025-06-30 14:44:17 +0530

    Force remove .env and reapply .gitignore

--- chattify-app/hooks/useAuth.ts
+++
@@ -1,7 +0,0 @@
-import { useAuth as useAuthContext } from '@/context/authContext';
-
-const useAuth = () => {
-  return useAuthContext();
-};
-
-export default useAuth;
--- chattify-server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
-import { searchUsers } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/', protect, searchUsers); // /api/users?search=example
-
-export default router;
